073c5c3c35f97267b3fd732462648739bf9bdf01
leobarbosadev <leobarbosajf@hotmail.com>
2025-10-01 21:39:28 -0300

    digitando o while

---
+++ aula005_while.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+void main(){
+  // --------------------------------
+  // Variável de controle do laço
+  int i = 1;
+
+
+  while (i <=5){
+
+    stdout.write('$i ');
+
+    i++;
+  }
+}
