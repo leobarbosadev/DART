a040df7520bf5eff00208ba582f35f467ca4096d
leobarbosadev <leobarbosajf@hotmail.com>
2025-10-19 17:07:04 -0300

    Finalizando atividades

---
+++ atividades/atividade_s.dart
@@ -0,0 +1,35 @@
+import 'dart:io';
+import 'dart:math';
+
+void main(){
+  stdout.write('Digite o valor do seu capital: ');
+  String? valorA = stdin.readLineSync();
+
+  stdout.write('Digite a taxa de juros ao mês (%): ');
+  String? valorB = stdin.readLineSync();
+
+  stdout.write('Digite o tempo em meses: ');
+  String? valorC = stdin.readLineSync();
+
+  double capital = double.parse(valorA!);
+  double taxa = double.parse(valorB!) / 100;
+  int tempo = int.parse(valorC!);
+  double jurosSimples;
+  double jurosCompostos;
+  double montanteFinal;
+
+  jurosSimples = capital * taxa * tempo;
+  montanteFinal = capital + jurosSimples;
+
+  jurosCompostos = capital * pow((1 + taxa), tempo);
+
+  print('');
+  print('== JUROS SIMPLES ==');
+  print('Um capital de R\$${capital} com uma taxa de juros de ${taxa * 100}% ao mês durante ${tempo} meses, no final terá R\$${montanteFinal.toStringAsFixed(2)}');
+  print('*' * 50);
+  print('== JUROS COMPOSTOS ==');
+  print('Um capital de R\$${capital} com uma taxa de juros de ${taxa * 100}% ao mês durante ${tempo} meses, no final terá R\$${jurosCompostos.toStringAsFixed(2)}');
+
+
+
+}
