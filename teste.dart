43bddd5a0e9eba6c705efeb7432cf34f2d8fbca2
leobarbosadev <leobarbosajf@hotmail.com>
2025-10-22 19:27:35 -0300

    Fazendo testes com while

--- teste.dart
+++ teste.dart
@@ -33,33 +33,81 @@
 //   }
 // }
 
+// //Recebe um numero e soma 5 a ele
+
+// import 'dart:io';
+
+// void main(){
+
+//   while(true){
+//     stdout.write('Entre com um número: ');
+//     String? entrada = stdin.readLineSync();
+    
+//     if(entrada != null && entrada.isNotEmpty){
+//       int? valor = int.tryParse(entrada); //Aqui tenta conververter a string para int
+//       int resultado;
+//       if(valor != null){ // CONSEGUIU FAZER O PARSE
+//         resultado = valor + 5;
+//         print('O número digitado foi ${valor}, somado 5 é igual a ${resultado}');
+//       }else{ // AQUI ELE NÃO CONSEGUIU FAZER O PARSE
+//         print('Digite somente números!!');
+//       }
+//     }
+    
+//     stdout.write('Deseja continuar? (s/n) ');
+//     String? resposta = stdin.readLineSync();
+
+//     if(resposta == null || resposta.isEmpty){
+//       print('valor invalido');
+//       continue;
+//     }
+
+//     if(resposta.toLowerCase() == 's'){
+//       continue;
+//     }else{
+//       print('Programa encerrado...');
+//       break;
+//     }
+//   }
+// }
+
+
+
+//VERIFICAR SE É PAR OU ÍMPAR
 import 'dart:io';
 
+
 void main(){
-
   while(true){
+    stdout.write('Entre com um valor: ');
+    String? entrada = stdin.readLineSync();
+
+    if(entrada != null && entrada.isNotEmpty){
+      int? valor = int.tryParse(entrada); // AQUI TENTA FAZER O CASTING PARA int
+      if(valor == null){ // AQUI NÃO FOI POSSÍVEL FAZER O CASTING
+        print('Valor inválido!');
+        continue;
+      }else{ // AQUI FOI FEITO O CASTING
+        if(valor % 2 == 0){
+          print('O número ${valor} é par!!!');
+        }else{
+          print('O número ${valor} é ímpar!!!');
+        }
+      }
+    }
 
 
-
-
-
-
-
-
-
-
-
-    stdout.write('Deseja continuar? (s/n)');
+    stdout.write('Deseja continuar? (s/n) ');
     String? resposta = stdin.readLineSync();
 
     if(resposta == null || resposta.isEmpty){
-      print('valor invalido');
+      print('valor inválido');
       continue;
     }
-
     if(resposta.toLowerCase() == 's'){
       continue;
     }else{
+      print('Programa encerrado!!!');
       break;
     }
   }
