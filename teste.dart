76833347e03f7e894eddb02a748f191579a05ce7
leobarbosadev <leobarbosajf@hotmail.com>
2025-10-26 17:16:32 -0300

    Fazendo alterações no arquivo de teste

--- teste.dart
+++ teste.dart
@@ -1,78 +1,3 @@
-// // //Recebe um numero e soma 5 a ele
-// import 'dart:io';
-
-
-// void main(){
-
-
-//   while(true){
-//     stdout.write('Entre com um número: ');
-//     String? val = stdin.readLineSync();
-
-//     if(val != null && val.isNotEmpty){
-//       int? valor = int.tryParse(val); //Se o tryParse tentar fazer a conversao para inteiro e não conseguir(digitar letra) vai retornar null
-//       int resultado;
-//       if(valor != null){ //Aqui o tryParse consegue fazer a conversão
-//         resultado = valor + 5;
-//         print('O valor digitado foi: ${valor}, esse valor mais 5 é igual a: ${resultado}');
-//       }else{ //Aqui ele não consegue fazer a conversão
-//         print('Só numero');
-//       }
-
-//     }else{
-//       print('Valor em branco ou nulo');
-//       continue;
-//     }
-//     stdout.write('Deseja finalizar? (s/n)');
-//     var continuar = stdin.readLineSync();
-
-//     if(continuar == 's' || continuar != null){
-//       print('Programa finalizado!!');
-//       break;
-//     }
-//   }
-// }
-
-// //Recebe um numero e soma 5 a ele
-
-// import 'dart:io';
-
-// void main(){
-
-//   while(true){
-//     stdout.write('Entre com um número: ');
-//     String? entrada = stdin.readLineSync();
-    
-//     if(entrada != null && entrada.isNotEmpty){
-//       int? valor = int.tryParse(entrada); //Aqui tenta conververter a string para int
-//       int resultado;
-//       if(valor != null){ // CONSEGUIU FAZER O PARSE
-//         resultado = valor + 5;
-//         print('O número digitado foi ${valor}, somado 5 é igual a ${resultado}');
-//       }else{ // AQUI ELE NÃO CONSEGUIU FAZER O PARSE
-//         print('Digite somente números!!');
-//       }
-//     }
-    
-//     stdout.write('Deseja continuar? (s/n) ');
-//     String? resposta = stdin.readLineSync();
-
-//     if(resposta == null || resposta.isEmpty){
-//       print('valor invalido');
-//       continue;
-//     }
-
-//     if(resposta.toLowerCase() == 's'){
-//       continue;
-//     }else{
-//       print('Programa encerrado...');
-//       break;
-//     }
-//   }
-// }
-
-
-
 //VERIFICAR SE É PAR OU ÍMPAR
 import 'dart:io';
 
@@ -84,11 +9,11 @@ void main(){
 
 
     if(entrada != null && entrada.isNotEmpty){
-      int? valor = int.tryParse(entrada); // AQUI TENTA FAZER O CASTING PARA int
-      if(valor == null || entrada.isEmpty){ // AQUI NÃO FOI POSSÍVEL FAZER O CASTING
+      int? valor = int.tryParse(entrada); // AQUI TENTA FAZER O PARSE PARA int
+      if(valor == null){ // AQUI NÃO FOI POSSÍVEL FAZER O PARSE
         print('Digite somente números!');
         continue;
-      }else{ // AQUI FOI FEITO O CASTING
+      }else{ // AQUI FOI FEITO O PARSE
         if(valor % 2 == 0){
           print('O número ${valor} é par!!!');
         }else{
@@ -96,8 +21,6 @@ void main(){
         }
       }
     }
-
-
     stdout.write('Deseja continuar? (s/n) ');
     String? resposta = stdin.readLineSync();
 
