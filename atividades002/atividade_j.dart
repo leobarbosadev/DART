dbdb875906aa3cc83167549e33efb0dd74ba6d4b
leobarbosadev <leobarbosajf@hotmail.com>
2025-10-27 21:39:42 -0300

    Corrigir exercicio k

--- atividades002/atividade_j.dart
+++ atividades002/atividade_j.dart
@@ -1,2 +1,40 @@
 //Contagem regressiva: Faça um programa que receba um número inteiro positivo e faça
 //uma contagem regressiva até zero, imprimindo cada número.
+import 'dart:io';
+
+
+void main(){
+   while(true){
+    print('=== CONTAGEM REGRESSIVA ===');
+    stdout.write('Entre com um número inteiro: ');
+    String? entrada = stdin.readLineSync();
+
+    if(entrada != null && entrada.isNotEmpty){
+      int? numero = int.tryParse(entrada);
+
+      if(numero == null){
+        print('Digite somente números');
+      }else{
+        for(int i = numero; i >= 0; i--){
+          print('Contagem regressiva: ${i}');
+          sleep(Duration(seconds:1));
+        }
+      }
+    }
+    print('');
+    stdout.write('Desja continuar? (s/n) ');
+    String? escolha = stdin.readLineSync();
+
+    if (escolha == null || escolha.trim().isEmpty) {
+      print('Valor inválido!!!');
+      print('');
+      continue;
+    }
+    if (escolha.toLowerCase() == 's') {
+      continue;
+    } else {
+      print('Programa encerrado!!!');
+      break;
+    }
+  }
+}
--- atividades002/atividade_k.dart
+++ atividades002/atividade_k.dart
@@ -1,2 +1,47 @@
 //Determinação do maior divisor comum (MDC): Faça um programa que receba dois
 //números inteiros e calcule o MDC usando o algoritmo de Euclides com repetição.
+import 'dart:io';
+
+
+void main(){
+  while(true){
+    print('=== MDC ===');
+    stdout.write('Entre com primeiro número inteiro: ');
+    String? entrada_1 = stdin.readLineSync();
+
+    stdout.write('Entre com o segundo número inteiro: ');
+    String? entrada_2 = stdin.readLineSync();
+
+    if(entrada_1 != null && entrada_1.isNotEmpty && entrada_2 != null && entrada_2.isNotEmpty){
+      int? primeiroValor = int.tryParse(entrada_1);
+      int? segundoValor = int.tryParse(entrada_2);
+
+      if(primeiroValor != null && segundoValor != null){
+        print('Digite somente números');
+        while(segundoValor != 0){
+          int resto = primeiroValor % segundoValor;
+          primeiroValor = segundoValor;
+          segundoValor = resto;
+        }
+      }else{
+        
+      print('O MDC de ${entrada_1} e ${entrada_2} é ${primeiroValor}');
+     }
+    }
+    print('');
+    stdout.write('Desja continuar? (s/n) ');
+    String? escolha = stdin.readLineSync();
+
+    if (escolha == null || escolha.trim().isEmpty) {
+      print('Valor inválido!!!');
+      print('');
+      continue;
+    }
+    if (escolha.toLowerCase() == 's') {
+      continue;
+    } else {
+      print('Programa encerrado!!!');
+      break;
+    }
+  }
+}
